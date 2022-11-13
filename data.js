3107e5849c514d27f38a4a9a5b2ee4e99419edc2
Alladoctor2001 <96998662+Alladoctor2001@users.noreply.github.com>
2022-11-13 17:01:09 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+let shopItemsData = [{
+    id : 'item1',
+    name :"Causal Shirt",
+    price : 45 ,
+    desc : "Best casual shirt for daily basis",
+    img : "images/img-1.jpg"
+},{
+    id : 'item2',
+    name :"Office Shirt",
+    price : 100  ,
+    desc : "Office shirt for Boulal services.",
+    img : "images/img-2.jpg"
+},{
+    id : 'item3',
+    name :"T Shirt",
+    price : 25 ,
+    desc : "T shirt during summer, when we go to the T, having a cup of tea",
+    img : "images/t-shirt.webp"
+},{
+    id : 'item4',
+    name :"Suit",
+    price : 300 ,
+    desc : "A suit for getting official awards while becoming a champion",
+    img : "images/suit.jpg"
+}]
